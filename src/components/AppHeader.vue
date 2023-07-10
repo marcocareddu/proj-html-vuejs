289b36bf6836749652adf8c631658db44c35ac22
marcocareddu <saldumaccu@tuta.io>
2023-07-10 17:15:25 +0200

    VUE AppHeader - Added base template & import data

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,43 @@
 <script>
-export default {}
+import SearchBar from './BaseSearchBar.vue';
+
+export default {
+    props: {
+        headerMenu: Object
+    },
+    components: { SearchBar },
+}
 </script>
 
-<template></template>
+<template>
+    <header>
+
+        <!-- Logo -->
+        <div class="logo">
+            <img src="" alt="MaxCoach Logo">
+        </div>
+
+        <!-- Navbar -->
+        <div class="navbar">
+
+            <!-- Navbar Menu -->
+            <div class="menu">
+                <ul>
+                    <li v-for="voice in headerMenu" :key="voice.title">
+                        <a :href="voice.title">{{ voice.title }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- User section -->
+        <div class="user">
+            <!-- !TODO Insert icon -->
+        </div>
+
+        <!-- SearchBar -->
+        <SearchBar />
+    </header>
+</template>
 
 <style></style>
