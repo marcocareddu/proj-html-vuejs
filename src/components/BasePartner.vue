00064796ec8ed947dc64942ae745a52edc87c347
marcocareddu <saldumaccu@tuta.io>
2023-07-10 20:33:17 +0200

    VUE BasePartner - Added base template

---
+++ src/components/BasePartner.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="container partner-row d-flex justify-content-evenly align-items-center">
+        <img v-for="image in getSixImagePath" :src="image" alt="#">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+    opacity: 0.5;
+    transition: 0.3s;
+
+    &:hover {
+        opacity: 1;
+    }
+}
+</style>
