2fff743ef7b36f60e7431748a725b9ecc44077a1
marcocareddu <saldumaccu@tuta.io>
2023-07-10 16:42:09 +0200

    VUE App - Imported components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
-<script></script>
+<script>
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 
-<template></template>
+export default {
+    name: 'MaxCoach',
+    components: {
+        AppMain,
+        AppHeader,
+        AppFooter,
+    },
+}
+</script>
+
+<template>
+    <!-- Header -->
+    <AppHeader />
+
+    <!-- Main -->
+    <AppMain />
+
+    <!-- Footer -->
+    <AppFooter />
+</template>
 
 <style lang="scss">
 @use './assets/scss/style.scss';
