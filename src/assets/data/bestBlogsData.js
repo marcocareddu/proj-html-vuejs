ffd8d26bd59c84d3b647ddd7dd672e56a1c51a5d
marcocareddu <saldumaccu@tuta.io>
2023-07-10 15:58:23 +0200

    Added bestBlogs Data

---
+++ src/assets/data/bestBlogsData.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        image: 'artist-blog-03-480x352.jpeg',
+        type: 'Artist',
+        title: 'Brush Strokes Energize Trees in Paintings',
+        data: 'May 15, 2020',
+        views: 688,
+    },
+    {
+        image: 'artist-blog-01-480x352.jpg',
+        type: 'Artist',
+        title: 'Pocket-Sized Notebooks Hold Miniature Paintings',
+        data: 'May 15, 2020',
+        views: 603,
+    },
+]
