b5041f163ee9ba03026b0d689e6ed4b63379a79b
marcocareddu <saldumaccu@tuta.io>
2023-07-10 18:01:16 +0200

    VUE App Header - Added temporary style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,34 +10,64 @@ export default {
 </script>
 
 <template>
-    <header>
+    <header class="">
+        <div class="container d-flex align-items-center justify-content-between h-100">
 
-        <!-- Logo -->
-        <div class="logo">
-            <img src="" alt="MaxCoach Logo">
-        </div>
-
-        <!-- Navbar -->
-        <div class="navbar">
-
-            <!-- Navbar Menu -->
-            <div class="menu">
-                <ul>
-                    <li v-for="voice in headerMenu" :key="voice.title">
-                        <a :href="voice.title">{{ voice.title }}</a>
-                    </li>
-                </ul>
+            <!-- Logo -->
+            <div class="logo">
+                <img src="../assets/img/dark-logo.png" alt="MaxCoach Logo">
             </div>
-        </div>
 
-        <!-- User section -->
-        <div class="user">
-            <!-- !TODO Insert icon -->
-        </div>
+            <!-- Navbar -->
+            <div class="navbar py-3">
 
-        <!-- SearchBar -->
-        <SearchBar />
+                <!-- Navbar Menu -->
+                <div class="menu">
+                    <ul class="d-flex mb-0">
+                        <li v-for="voice in headerMenu" :key="voice.title" class="pe-3">
+                            <a :href="voice.title">{{ voice.title }}</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- User section -->
+                <div class="user">
+                    <!-- !TODO Insert icon -->
+                </div>
+
+                <!-- SearchBar -->
+                <SearchBar />
+            </div>
+
+
+        </div>
     </header>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/_vars.scss' as *;
+
+header {
+    height: 80px;
+    background: $mainBgColor;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    box-shadow: 0px 0px 20px 0px #63F2FF;
+}
+
+
+img {
+    height: 30px;
+}
+
+ul li {
+    list-style-type: none;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+</style>
