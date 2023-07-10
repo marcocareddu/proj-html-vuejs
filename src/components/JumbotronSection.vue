64a8a9eaf4a65ea1037492987476a0a537b80ab7
marcocareddu <saldumaccu@tuta.io>
2023-07-10 18:46:27 +0200

    Vue JumbotronSection - Created Base template

--- src/components/JumbotronSection.vue
+++ src/components/JumbotronSection.vue
@@ -4,19 +4,58 @@ export default {}
 
 <template>
     <div class="jumbotron">
-        <div class="container d-flex">
+        <div class="container d-flex justify-content-between py-5">
 
             <!-- Text Section -->
             <div class="text-box">
+                <h1 class="fw-bolder">Distant learning for further expansion</h1>
+                <p class="py-2">Learning is a life-long journey that in fact we never find the termonate stop. Stop
+                    searching, enjoy the
+                    process.</p>
 
+                <!-- ! INSERT BASE BUTTON COMPONENT-->
+                <baseButton />
+
+                <!-- ! TEMPORARY -->
+                <button class='my-3'>Download free guide</button>
+
+                <p class="py-2">Have questions? <a href="#" class="fw-bolder">Get Free Samples</a></p>
             </div>
 
             <!-- Picture section -->
-            <div class="picture-section"></div>
+            <div class="picture-section d-flex justify-content-center align-items-center">
+                <img src="../assets/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="">
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-@use '../assets/scss/partials/vars';
+@use '../assets/scss/partials/vars' as *;
+
+.jumbotron {
+    color: $tertiaryColor;
+    margin-top: 80px;
+}
+
+.text-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 40%;
+
+    a {
+        text-decoration: none;
+        color: $secondaryColor;
+    }
+}
+
+h1 {
+    font-size: 50px;
+}
+
+img {
+    object-fit: contain;
+    border-radius: 100%;
+}
 </style>
