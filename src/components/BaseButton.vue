250628d20381e725633d9c41eaf4ef9069e75cbc
marcocareddu <saldumaccu@tuta.io>
2023-07-10 20:56:51 +0200

    VUE BaseButton - Temporary styled btn component

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,24 @@
 <script>
-export default {}
+export default {
+    props: { buttonText: String }
+}
 </script>
 
-<template></template>
+<template>
+    <button class='my-3 px-5 py-3 rounded border-0'>{{ buttonText }}</button>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/vars' as *;
+
+button {
+    color: white;
+    font-weight: 600;
+    background-color: $secondaryColor;
+    transition: 0.3s;
+
+    &:hover {
+        background-color: $tertiaryColor;
+    }
+}
+</style>
