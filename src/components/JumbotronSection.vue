b894399446cb54bfbe3b1cf6770e43ec04662cb3
marcocareddu <saldumaccu@tuta.io>
2023-07-10 18:11:13 +0200

    VUE JumbotronSectrion - Added base template

--- src/components/JumbotronSection.vue
+++ src/components/JumbotronSection.vue
@@ -2,6 +2,21 @@
 export default {}
 </script>
 
-<template></template>
+<template>
+    <div class="jumbotron">
+        <div class="container d-flex">
 
-<style></style>
+            <!-- Text Section -->
+            <div class="text-box">
+
+            </div>
+
+            <!-- Picture section -->
+            <div class="picture-section"></div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/vars';
+</style>
