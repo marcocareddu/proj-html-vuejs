5c5d3f921708dd4cca3cafa13458678d9d6f1fa3
marcocareddu <saldumaccu@tuta.io>
2023-07-11 14:59:44 +0200

    VUE CarouselSection - Imported data & props

--- src/components/carousel/CarouselSection.vue
+++ src/components/carousel/CarouselSection.vue
@@ -1,13 +1,23 @@
 <script>
 import CarouselCard from './CarouselCard.vue';
 
+// Import Data
+import { carouselData } from '../../assets/data/index';
+
 export default {
     components: { CarouselCard },
+
+    data() {
+        return {
+            carouselData,
+        }
+    }
 }
+
 </script>
 
 <template>
-    <CarouselCard />
+    <CarouselCard v-for="card in carouselData" :key="card.name" :carouselData="card" />
 </template>
 
 <style></style>
