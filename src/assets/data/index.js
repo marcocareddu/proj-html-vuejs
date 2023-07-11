079ddfbe0abe4c64c5bae83538999b01e65aa391
marcocareddu <saldumaccu@tuta.io>
2023-07-11 17:21:25 +0200

    JS index- Fixed name position

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,21 +1,21 @@
 import addressData from './_addressData';
 import bestBlogsData from './_bestBlogsData';
+import bestBlogsList from './_bestBlogsList';
 import carouselData from './_carouselData';
 import footerMenu from './_footerMenu';
 import headerMenu from './_headerMenu';
 import courseData from './_courseData';
 import tutoringData from './_tutoringData';
 import achievementsData from './_achievementsData';
-import bestBlogsList from './_bestBlogsList';
 
 export {
     addressData,
     bestBlogsData,
+    bestBlogsList,
     carouselData,
     footerMenu,
     headerMenu,
     courseData,
     tutoringData,
     achievementsData,
-    bestBlogsList,
 };
