bdfcdb87ddc2e32cbe3266f4467a0c04b443f4df
marcocareddu <saldumaccu@tuta.io>
2023-07-11 11:45:36 +0200

    VUE CoursesSection - Added  Style & Import Card

--- src/components/courses/CoursesSection.vue
+++ src/components/courses/CoursesSection.vue
@@ -1,11 +1,13 @@
 <script>
 import CoursesCard from '../courses/CoursesCard.vue'
+import BaseButton from '../BaseButton.vue';
+
 
 // Import Data
 import { courseData } from '../../assets/data/index';
 
 export default {
-    components: { CoursesCard },
+    components: { CoursesCard, BaseButton },
 
     data() {
         return {
@@ -16,12 +18,42 @@ export default {
 </script>
 
 <template>
-    <div class="container py-4">
-        <div class="row">
-            <!-- Courses Card -->
-            <CoursesCard v-for="card in courseData" :courseData="card" />
+    <!-- Courses Section -->
+    <div class="courses-section">
+
+        <div class="container p-4">
+            <div class="title text-center py-5">
+                <h6>STIMULATED TO LEARN?</h6>
+                <h3 class="fw-bolder">Featured Online Courses</h3>
+            </div>
+
+            <!-- Courses card section -->
+            <div class="row">
+
+                <!-- Courses Cards -->
+                <CoursesCard v-for="card in courseData" :courseData="card" />
+            </div>
+
+            <!-- Button -->
+            <div class="text-center">
+                <BaseButton :buttonText="'View all courses'" />
+            </div>
         </div>
     </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/_vars.scss' as *;
+
+.courses-section {
+    background-color: $onlineCoursesBg;
+
+    h6 {
+        color: $customLilla;
+    }
+}
+
+.title {
+    color: $tertiaryColor;
+}
+</style>
