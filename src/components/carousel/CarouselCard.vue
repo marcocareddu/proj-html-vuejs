be3a9d581e8d92249f902463c615b0604db501e5
marcocareddu <saldumaccu@tuta.io>
2023-07-11 15:17:40 +0200

    VUE CarouselCard - Added Temporary template

--- src/components/carousel/CarouselCard.vue
+++ src/components/carousel/CarouselCard.vue
@@ -5,7 +5,20 @@ export default {
 </script>
 
 <template>
-    <h1>{{ carouselData.title }}</h1>
+    <div class="col-3">
+        <div class="text">
+            <h6>{{ carouselData.title }}</h6>
+            <p> {{ carouselData.text }}</p>
+        </div>
+        <div class="d-flex col-6 px-5 py-3 align-items-center">
+            <div class="px-3">
+                <img :src="`./src/assets/img/${carouselData.image}`" alt="">
+            </div>
+            <div class="px-2">
+                <h4 class="fw-bolder">{{ carouselData.name.toUpperCase() }}</h4>
+                <h5>/ {{ carouselData.job }}</h5>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style></style>
