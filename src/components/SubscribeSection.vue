6210c29fb0c3695b7c2ac9e5125379d8c6297cad
marcocareddu <saldumaccu@tuta.io>
2023-07-11 17:55:11 +0200

    VUE SubscribeSection - Added components % search

---
+++ src/components/SubscribeSection.vue
@@ -0,0 +1,13 @@
+<script>
+import SearchBar from './BaseSearchBar.vue';
+
+export default {
+    components: { SearchBar },
+}
+</script>
+
+<template>
+    <SearchBar />
+</template>
+
+<style></style>
