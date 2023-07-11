f7b5176ed827275e474d9da1ce33e5ac5e157559
marcocareddu <saldumaccu@tuta.io>
2023-07-11 15:00:22 +0200

    VUE CarouselData - Added props

--- src/components/carousel/CarouselCard.vue
+++ src/components/carousel/CarouselCard.vue
@@ -1,7 +1,11 @@
 <script>
-export default {}
+export default {
+    props: { carouselData: Object }
+}
 </script>
 
-<template></template>
+<template>
+    <h1>{{ carouselData.title }}</h1>
+</template>
 
 <style></style>
