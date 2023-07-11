d99c6485e1624e622d11afa3b87550473bc3c239
marcocareddu <saldumaccu@tuta.io>
2023-07-11 16:41:05 +0200

    VUE AchievementsSection - Added base template

--- src/components/achievements/AchievementsSection.vue
+++ src/components/achievements/AchievementsSection.vue
@@ -8,15 +8,18 @@ export default {
     components: { AchievementsCard },
 
     data() {
-        return {
-            achievementsData,
-        }
+        return { achievementsData }
     }
 }
 </script>
 
 <template>
-    <AchievementsCard v-for="card in achievementsData" :achievementsData="card" />
+    <div class="achievements-section"></div>
+    <div class="container">
+        <div class="row">
+            <AchievementsCard v-for="card in achievementsData" :achievementsData="card" />
+        </div>
+    </div>
 </template>
 
 <style></style>
