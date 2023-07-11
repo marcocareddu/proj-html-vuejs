83001a62b7695768da4b6af1bfcb93ccf38729d3
marcocareddu <saldumaccu@tuta.io>
2023-07-11 13:03:05 +0200

    VUE Companionship - Added animation

--- src/components/companionship/CompanionshipSection.vue
+++ src/components/companionship/CompanionshipSection.vue
@@ -5,7 +5,7 @@ export default {}
 <template>
     <div class="container d-flex py-5 justify-content-evenly">
         <div class="picture px-5">
-            <img src="../../assets/img/home-2-popup-video-poster.jpg" class="img-fluid rounded-2" alt="">
+            <img src="../../assets/img/home-2-popup-video-poster.jpg" class="rounded-2" alt="">
             <img src="../../assets/img/icon-youtube-play.png" class="youtube img-fluid" alt="">
         </div>
 
@@ -26,10 +26,16 @@ export default {}
 .picture {
     position: relative;
     cursor: pointer;
+    overflow: hidden;
 }
 
 img {
     height: 450px;
+    transition: 1s;
+
+    &:hover {
+        transform: scale(1.1);
+    }
 }
 
 .youtube {
@@ -48,7 +54,6 @@ img {
 h1,
 h6 {
     color: $tertiaryColor;
-
 }
 
 .companionship,
