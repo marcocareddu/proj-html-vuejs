a527c3995f5004a3dffb563c69fd064ccfc33f58
marcocareddu <saldumaccu@tuta.io>
2023-07-11 17:22:12 +0200

    VUE BestblogsCard - Added base style & template

--- src/components/best_blogs/BestblogsCard.vue
+++ src/components/best_blogs/BestblogsCard.vue
@@ -1,7 +1,45 @@
 <script>
-export default {}
+export default {
+    props: { bestblogsData: Object }
+}
 </script>
 
-<template></template>
+<template>
+    <div class="custom-card col-4 bg-white">
+        <div class="">
+            <img :src="`./src/assets/img/${bestblogsData.image}`" class="img-fluid" alt="">
+        </div>
+        <div class="px-2 info-box">
+            <h6>{{ bestblogsData.type.toUpperCase() }}</h6>
+            <h4 class="fw-bolder">{{ bestblogsData.title }}</h4>
+            <div class="d-flex info-lessons">
+                <p class="pe-3">{{ bestblogsData.data }}</p>
+                <p>{{ bestblogsData.views }} views</p>
+            </div>
+        </div>
+    </div>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/_vars.scss' as *;
+
+h4 {
+    color: $tertiaryColor;
+}
+
+h6 {
+    color: $customLilla;
+}
+
+p {
+    color: $customDarkGrey;
+    font-size: 1rem;
+}
+
+a {
+    text-decoration: none;
+    color: $customLilla;
+    font-size: 0.8rem;
+    transition: 0.3s;
+}
+</style>
