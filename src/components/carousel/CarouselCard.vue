8e345a8a3ac41f13130213e69f9e91a85cd2f94c
marcocareddu <saldumaccu@tuta.io>
2023-07-11 15:35:14 +0200

    VUE CarouselCard - Static carousel added style

--- src/components/carousel/CarouselCard.vue
+++ src/components/carousel/CarouselCard.vue
@@ -7,15 +7,15 @@ export default {
 <template>
     <div class="col-3">
         <div class="text">
-            <h6>{{ carouselData.title }}</h6>
+            <h4 class="fw-bolder">{{ carouselData.title }}</h4>
             <p> {{ carouselData.text }}</p>
         </div>
-        <div class="d-flex col-6 px-5 py-3 align-items-center">
-            <div class="px-3">
+        <div class="d-flex py-3 align-items-center justify-content-start">
+            <div class="pe-3">
                 <img :src="`./src/assets/img/${carouselData.image}`" alt="">
             </div>
-            <div class="px-2">
-                <h4 class="fw-bolder">{{ carouselData.name.toUpperCase() }}</h4>
+            <div class="">
+                <h4 class="fw-bolder py-2">{{ carouselData.name.toUpperCase() }}</h4>
                 <h5>/ {{ carouselData.job }}</h5>
             </div>
         </div>
