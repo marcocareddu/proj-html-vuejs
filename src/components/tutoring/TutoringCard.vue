7e5589c873b790752c6e79e3d8b9ea0587198b98
marcocareddu <saldumaccu@tuta.io>
2023-07-11 11:46:00 +0200

    VUE TutoringCard - Added BaseTemplate

--- src/components/tutoring/TutoringCard.vue
+++ src/components/tutoring/TutoringCard.vue
@@ -1,7 +1,37 @@
 <script>
-export default {}
+export default {
+    props: { tutoringData: Object }
+}
 </script>
 
-<template></template>
+<template>
+    <div class="custom-card col-4 px-5 py-3">
+        <div class="px-3">
+            <img :src="`./src/assets/img/${tutoringData.image}`" alt="">
+        </div>
+        <div class="px-2 info-box">
+            <h4 class="fw-bolder">{{ tutoringData.title }}</h4>
+            <h5 class="fw-bolder">{{ tutoringData.text }}</h5>
+            <a :href="tutoringData.text">{{ tutoringData.textLink }}</a>
+        </div>
+    </div>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/_vars.scss' as *;
+
+h4 {
+    color: $secondaryColor;
+}
+
+h5 {
+    font-size: 1.2rem;
+}
+
+img {
+    height: 150px;
+    width: 150px;
+    object-fit: cover;
+    border-radius: 100%;
+}
+</style>
