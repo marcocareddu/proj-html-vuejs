049d610a9334ab508e98ba7ed2e5e8e7cfc66a0a
marcocareddu <saldumaccu@tuta.io>
2023-07-11 20:48:53 +0200

    Aded icons

--- src/main.js
+++ src/main.js
@@ -12,13 +12,23 @@ import App from './App.vue'
 // Import Font Awsome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faMagnifyingGlass, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass, faChevronDown, faArrowRight, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser, faUser, faFileLines, faCalendar } from '@fortawesome/free-regular-svg-icons';
+import { faSquareFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 library.add(
     faMagnifyingGlass,
     faChevronDown,
-    faCircleUser
+    faCircleUser,
+    faUser,
+    faFileLines,
+    faArrowRight,
+    faCalendar,
+    faEye,
+    faSquareFacebook,
+    faTwitter,
+    faInstagram,
+    faLinkedin
 );
 
 createApp(App)
