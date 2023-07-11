ae29c057bbc44a213a3550b2a703869672b298fa
marcocareddu <saldumaccu@tuta.io>
2023-07-11 11:46:50 +0200

    VUE TutoringSection - Added style & import Card

--- src/components/tutoring/TutoringSection.vue
+++ src/components/tutoring/TutoringSection.vue
@@ -1,7 +1,39 @@
 <script>
-export default {}
+import TutoringCard from './TutoringCard.vue';
+
+// Import Data
+import { tutoringData } from '../../assets/data/index';
+
+export default {
+    components: { TutoringCard },
+
+    data() {
+        return {
+            tutoringData,
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <!-- Courses Section -->
+    <div class="tutoring-section">
+
+        <div class="container p-4">
+
+            <div class="title text-center py-5">
+                <h6>MAXCOACH AVAILABLE COURSES</h6>
+                <h3 class="fw-bolder">Access <span>Smart Tutoring</span> Program For Benefits.</h3>
+            </div>
+
+            <!-- Courses card section -->
+            <div class="row">
+
+                <!-- Courses Cards -->
+                <TutoringCard v-for="card in tutoringData" :tutoringData="card" />
+            </div>
+        </div>
+    </div>
+</template>
 
 <style></style>
