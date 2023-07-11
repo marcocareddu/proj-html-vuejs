9da089ce432e5f579893e5b268a9785b80a5c568
marcocareddu <saldumaccu@tuta.io>
2023-07-11 18:41:19 +0200

    VUE AppFooter - Added address

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,40 @@
 <script>
-export default {}
+
+// Import data
+import addressData from '../assets/data/_addressData';
+import footerMenu from '../assets/data/_footerMenu';
+
+export default {
+    data() {
+        return {
+            addressData,
+            footerMenu,
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <div class="container">
+            <div>
+                <div class="address">
+                    <h2>Address</h2>
+                    <ul>
+                        <li v-for="text in addressData">{{ text }}</li>
+                    </ul>
+                </div>
+            </div>
 
-<style></style>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/vars' as *;
+
+footer {
+    background-color: $topsBgColor;
+    height: 500px;
+}
+</style>
