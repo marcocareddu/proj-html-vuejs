e01e17ed6c100e5abb5d30ea110f54b2c4de39bf
marcocareddu <saldumaccu@tuta.io>
2023-07-11 12:49:11 +0200

    VUE Companionshio - Added base template

--- src/components/companionship/CompanionshipSection.vue
+++ src/components/companionship/CompanionshipSection.vue
@@ -2,6 +2,22 @@
 export default {}
 </script>
 
-<template></template>
+<template>
+    <div class="container d-flex py-5 justify-content-evenly">
+        <div class="picture px-5">
+            <img src="../../assets/img/home-2-popup-video-poster.jpg" class="img-fluid rounded-2" alt="">
+            <img src="../../assets/img/icon-youtube-play.png" class="youtube img-fluid" alt="">
+        </div>
 
-<style></style>
+        <div class="text d-flex flex-column align-self-center">
+            <h6>GET STARTED <span class="fw-bolder">EFFORTLESSLY</span></h6>
+            <h1 class="fw-bolder pt-3">Enjoy Our</h1>
+            <h1 class="companionship">Companionship</h1>
+            <p>Sneek peek into what has in store for you on our online teaching and mentoring portal. Seek for some hidden
+                hints of early-bird rewards as well.</p>
+            <a href="#" class="fw-bolder">Download free guidebook</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
