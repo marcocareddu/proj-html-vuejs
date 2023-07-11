21b73501fa05887efac7dd00c8f51d8e5a5cc398
marcocareddu <saldumaccu@tuta.io>
2023-07-11 16:20:18 +0200

    VUE Achievemnts - Imported card & data

--- src/components/achievements/AchievementsSection.vue
+++ src/components/achievements/AchievementsSection.vue
@@ -1,7 +1,22 @@
 <script>
-export default {}
+import AchievementsCard from './AchievementsCard.vue';
+
+// Import Data
+import { achievementsData } from '../../assets/data/index';
+
+export default {
+    components: { AchievementsCard },
+
+    data() {
+        return {
+            achievementsData,
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <AchievementsCard v-for="card in achievementsData" :achievementsData="card" />
+</template>
 
 <style></style>
