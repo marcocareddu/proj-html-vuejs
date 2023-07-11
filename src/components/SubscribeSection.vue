5a464f2bd84f8654fce8cb91127dfe73f7f30229
marcocareddu <saldumaccu@tuta.io>
2023-07-11 18:22:52 +0200

    VUE SubscriptionSection - Added Base template

--- src/components/SubscribeSection.vue
+++ src/components/SubscribeSection.vue
@@ -1,13 +1,26 @@
 <script>
-import SearchBar from './BaseSearchBar.vue';
+import BaseButton from './BaseButton.vue';
 
 export default {
-    components: { SearchBar },
+    components: { BaseButton },
 }
 </script>
 
 <template>
-    <SearchBar />
+    <div class="subscribe-section">
+        <div class="container text-center">
+
+            <div class="mini-container">
+                <h2 class="fw-bolder">Subscribe <span class="fw-normal">Newsletters</span></h2>
+                <h5>Enter your email address to register to our newsletter subscription
+                    delivered on a regular basis!</h5>
+                <form>
+                    <input type="email" class='px-3 py-3 rounded-start border-0' placeholder="Enter your email">
+                    <BaseButton :buttonText="'Subscribe'" class="rounded-start-0" />
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped></style>
