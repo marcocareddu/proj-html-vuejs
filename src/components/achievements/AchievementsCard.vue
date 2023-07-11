1529a0ab1b52c1eac1d5cee2e00537d864f6347b
marcocareddu <saldumaccu@tuta.io>
2023-07-11 16:20:46 +0200

    VUE AchievementsCard - Imported props & template

--- src/components/achievements/AchievementsCard.vue
+++ src/components/achievements/AchievementsCard.vue
@@ -1,7 +1,15 @@
 <script>
-export default {}
+export default {
+    props: { achievementsData: Object }
+}
 </script>
 
-<template></template>
+<template>
+    <div class="customCard">
+        <h3>{{ achievementsData.title }}</h3>
+        <h1>{{ achievementsData.number }}</h1>
+        <h4>{{ achievementsData.type }}</h4>
+    </div>
+</template>
 
 <style></style>
