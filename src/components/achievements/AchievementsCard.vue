68d6b2cbcd2dd7bbd250904dca27ca1aa1851e73
marcocareddu <saldumaccu@tuta.io>
2023-07-11 16:40:34 +0200

    VUE AchievemtnsCard - Added Style

--- src/components/achievements/AchievementsCard.vue
+++ src/components/achievements/AchievementsCard.vue
@@ -5,11 +5,31 @@ export default {
 </script>
 
 <template>
-    <div class="customCard">
-        <h3>{{ achievementsData.title }}</h3>
-        <h1>{{ achievementsData.number }}</h1>
-        <h4>{{ achievementsData.type }}</h4>
+    <div class="customCard py-5 text-center d-flex flex-column justify-content-center">
+        <h5 class="fw-bolder px-5">{{ achievementsData.title }}</h5>
+        <h1 class="display-3 fw-bold">{{ achievementsData.number }}</h1>
+        <h6 class="fw-bolder">{{ achievementsData.type }}</h6>
     </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/_vars.scss' as *;
+
+.customCard {
+    height: 350px;
+    border: 2px solid $topsBgColor;
+    flex-basis: 25%;
+}
+
+h5 {
+    color: $tertiaryColor;
+}
+
+h1 {
+    color: $secondaryColor;
+}
+
+h6 {
+    color: $customLilla;
+}
+</style>
