039527cfea81e054ab904d196db0f59bf6ec35b9
marcocareddu <saldumaccu@tuta.io>
2023-07-12 17:21:20 +0200

    VUE Jumbotron  Added bg images

--- src/components/JumbotronSection.vue
+++ src/components/JumbotronSection.vue
@@ -34,7 +34,13 @@ export default {
 
             <!-- Picture section -->
             <div class="picture-section d-flex justify-content-center align-items-center">
-                <img src="../assets/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="Girl Photo">
+                <img src="../assets/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="Girl Photo"
+                    class="main-photo">
+                <img src="../assets/img/maxcoach-shape-08.png" alt="" class="bg-1">
+                <img src="../assets/img/maxcoach-shape-08.png" alt="" class="bg-2">
+                <img src="../assets/img/maxcoach-shape-02.png" alt="" class="bg-3">
+                <img src="../assets/img/maxcoach-shape-07.png" alt="" class="bg-4">
+                <img src="../assets/img/maxcoach-shape-01.png" alt="" class="bg-5">
             </div>
         </div>
 
@@ -51,8 +57,39 @@ export default {
     margin-top: 80px;
 }
 
-.test {
-    width: 60%;
+.picture-section {
+    position: relative;
+}
+
+.bg-1 {
+    position: absolute;
+    top: 60px;
+    left: -60px;
+}
+
+.bg-2 {
+    position: absolute;
+    top: 210px;
+    left: -60px;
+}
+
+.bg-3 {
+    position: absolute;
+    top: 90px;
+    right: -20px;
+    z-index: 1;
+}
+
+.bg-4 {
+    position: absolute;
+    bottom: 0px;
+    right: 10px;
+}
+
+.bg-5 {
+    position: absolute;
+    bottom: 20px;
+    left: -60px;
 }
 
 .text-box {
@@ -71,8 +108,9 @@ h1 {
     font-size: 50px;
 }
 
-img {
+.main-photo {
     object-fit: contain;
     border-radius: 100%;
+    z-index: 1;
 }
 </style>
