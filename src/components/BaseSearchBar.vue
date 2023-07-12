fe6efa720fef4a7091bc67500e0454dd898d1494
marcocareddu <saldumaccu@tuta.io>
2023-07-12 17:51:03 +0200

    VUE InputSearch - Fixed color bg & added emit

--- src/components/BaseSearchBar.vue
+++ src/components/BaseSearchBar.vue
@@ -1,10 +1,12 @@
 <script>
 import { faFontAwesome } from '@fortawesome/free-solid-svg-icons';
-export default {}
+export default {
+    data() { return { stringToSearch: '' } },
+}
 </script>
 
 <template>
-    <form action="">
+    <form @submit.prevent="$emit('form-submit', stringToSearch)">
         <input type="text" class='p-3 rounded-start border-0' placeholder="Search...">
         <button class="rounded-end my-3 px-3 py-3 border-0"><font-awesome-icon
                 :icon="['fas', 'magnifying-glass']" /></button>
@@ -20,6 +22,16 @@ input {
 
     &:focus-visible {
         outline: none;
+    }
+
+    &:focus {
+        background-color: white;
+        border: 1px solid red;
+
+        button {
+            background-color: white;
+            border: 1px solid red
+        }
     }
 }
 
